c16b672ba7ab09a12686fcc1d61dad1e212336ba
Dammy <111135894+Emperor-Shadow@users.noreply.github.com>
2023-07-22 01:59:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ btn.addEventListener("click" , ()=>{
   for (d of view) {
     d.style.background = "transparent";
   }
+let dot = document.querySelectorAll(".dot");
+  for (dot of dot) {
+    dot.style.display = "none";
+  }
   
   document.querySelector(".sp").style.display = "none";
   })
